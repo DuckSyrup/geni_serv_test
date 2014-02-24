e7d3e773198828cf5d538d8bd84a049eb4c677c7
Manic <basbalmanic@gmail.com>
2014-02-23 19:27:50 -0800

    Returning the request.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var util = require('util');
+var circularJSON = require('circular-json');
 
 app.get('/*', function(req, res){
-	res.setHeader('Content-Type', 'application/json');
-	res.end('Hello, GENI user!\n\n'+JSON.stringify(req.ip,null,3)+'\n\n'+JSON.stringify(req.host,null,3)+'\n\n'+JSON.stringify(req.protocol,null,3));
+	var newObj = {};
+	newObj.req = req;
+	newObj.ip = req.ip;
+	res.send(circularJSON.stringify(newObj));
 });
+
+
+
 
 app.listen(3000);
 console.log('Listening on 3000');
